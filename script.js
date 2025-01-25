5b857797cfc3c3eacc5013b8c89eb89ebf9ca4f8
Evan <24954397+AngryGrizzlyBear@users.noreply.github.com>
2025-01-25 14:08:29 -0800

    Making a Button for Phone Size

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleDropdown(){
+    const content = document.querySelector('.dropdown-content');
+    // Toggle the 'show' class for the animation
+    content.classList.toggle('show');
+}
